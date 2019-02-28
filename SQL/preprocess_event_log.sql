0df2e7aaed92abdabeaa4d817d8979a4abd57359
Janos Poor <poorjanos@gmail.com>
2019-02-28 12:17:08 +0100

    Alter event log preprocessing

--- SQL/preprocess_event_log.sql
+++ SQL/preprocess_event_log.sql
@@ -1,31 +1,29 @@
-/* Formatted on 2019. 02. 27. 11:59:49 (QP5 v5.115.810.9015) */
-/* Preprocess CLAIMS logdata */
-
 /* Gen base table and rename cols */
 DROP TABLE T_CLAIMS_PA_OUTPUT;
 COMMIT;
 
-CREATE TABLE T_CLAIMS_PA_OUTPUT
-AS
-     SELECT   DISTINCT
-              f_paid AS case_id,
-              f_idopont AS event_end,
-              wflog_user AS user_id,
-              REPLACE (hun1, '->', ' ide: ') AS activity_hu,
-              REPLACE (hun1eng, '->', ' to ') AS activity_en,
-              CASE WHEN hun1 LIKE 'FKR %' THEN 'FAIRKAR' ELSE 'KONTAKT' END
-                 AS activity_type
-       FROM   mesterr.export_pa_wflog3
-      WHERE       (wflog_user LIKE 'OKK/%' OR hun1 LIKE 'FKR %')
-              AND (f_paid LIKE 'K-2017%' OR f_paid LIKE 'K-2018%')
-              AND hun1 IS NOT NULL
-   ORDER BY   F_PAID, F_IDOPONT;
+CREATE TABLE T_CLAIMS_PA_OUTPUT AS
+SELECT   DISTINCT
+         f_paid AS case_id,
+         f_idopont AS event_end,
+         wflog_user AS user_id,
+         REPLACE (hun1, '->', ' ide: ') AS activity_hu,
+         REPLACE (hun1eng, '->', ' to ') AS activity_en,
+         CASE WHEN hun1 LIKE 'FKR %' THEN 'FAIRKAR' ELSE 'KONTAKT' END
+            AS activity_type
+  FROM   mesterr.export_pa_wflog3
+ WHERE   REGEXP_LIKE (f_paid, 'K-201[78]/.*');
+ 
 
+/* Delete non-OKK and empty rows */
+DELETE FROM   T_CLAIMS_PA_OUTPUT
+      WHERE      (user_id NOT LIKE 'OKK/%'
+              AND activity_hu NOT LIKE 'FKR %')
+              OR  activity_hu IS NULL;
 COMMIT;
 
 
 /* Define then drop cases with first event outside 201701.01. and 2018.12.01.*/
-
 DELETE FROM   T_CLAIMS_PA_OUTPUT
       WHERE   case_id IN
                     (SELECT   case_id
